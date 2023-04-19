f4e983e7df70d3b5304522f410f910615e6ee0aa
Nithya <nithyashree@gmail.com>
2023-04-19 11:33:43 +0530

    change3

--- nithya.java
+++ nithya.java
@@ -3,7 +3,7 @@ public class nithya
 	public static void main(Stiring [] args)
 	{
 		System.out.println("Hello world")
-		System.out.println("Welcome to spt")
+		System.out.println("Welcome to spt. Batawadi")
 	}
 }
 
