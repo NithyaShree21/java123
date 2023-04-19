ac503e28d20e8af77ad83583077d66197aa479f4
Nithya <129258526+NithyaShree21@users.noreply.github.com>
2023-04-19 12:16:38 +0530

    Update nithya.java

--- nithya.java
+++ nithya.java
@@ -1,6 +1,6 @@
 public class nithya
 {
-	public static void main(Stiring [] args)
+	public static void main(String [] args)
 	{
 		System.out.println("Hello world")
 		System.out.println("Welcome to spt. Batawadi,Tumkur")
